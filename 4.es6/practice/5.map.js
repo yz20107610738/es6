93de03c95f9230df4f24db19234dae3d3cbd2449
siyuzhao <yz20107610738@sina.cn>
2020-02-11 17:40:08 +0800

    little fix

--- 4.es6/practice/5.map.js
+++ 4.es6/practice/5.map.js
@@ -1,12 +1,12 @@
 // map 我们叫他 哈希表  映射
 // key 值不能重复
 
-let o = {b : 2}
+let o = { b: 2 };
 let map = new Map();
 map.set({ a: 1 }, "123");
 map.set(1, "456");
 map.set({ a: 1 }, 123);
-map.set(o, 222)
+map.set(o, 222);
 
 console.log(map.get(1));
 
